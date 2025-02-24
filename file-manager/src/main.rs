093a9af1d6b63ff406532d9beefe62bb61077037
Alessandro Nerla <nerla.alessandro@gmail.com>
2025-02-24 20:23:09 +0100

    feat(core): implemented `FileMetadata`

--- file-manager/src/main.rs
+++ file-manager/src/main.rs
@@ -1,10 +1,10 @@
-use std::path::PathBuf;
 use shelf::shelf::Shelf;
+use std::path::PathBuf;
 
 mod shelf;
 
 fn main() {
     println!("Hello, world!");
     // Test
-    // Shelf::new(PathBuf::from("C:\\Users\\Alessandro\\Desktop\\Projects\\Tag-Based File Manager\\Automated Test Procedures\\Tests\\Workspaces\\Generated\\Directory"));
+    Shelf::new(PathBuf::from("C:\\Users\\Alessandro\\Desktop\\Projects\\Tag-Based File Manager\\Automated Test Procedures\\Tests\\Workspaces\\Generated\\Directory"));
 }
--- file-manager/src/shelf/shelf.rs
+++ file-manager/src/shelf/shelf.rs
@@ -1,10 +1,19 @@
 use chrono::{DateTime, Utc};
 use std::collections::{BTreeSet, HashMap, HashSet};
 use std::fs::{self, DirEntry};
+use std::os::windows::fs::MetadataExt;
 use std::path::PathBuf;
 use std::rc::Rc;
 use std::result::Result;
+use std::time::UNIX_EPOCH;
 use std::{io, rc};
+/*
+#[cfg(unix)]
+use std::os::unix::fs::MetadataExt as unixMetadata;
+#[cfg(windows)]
+use std::os::windows::fs::MetadataExt as windowsMetadata;
+*/
+
 // Shelf
 #[derive(Debug)]
 pub struct Shelf {
@@ -53,7 +62,8 @@ impl Node {
         let entries = fs::read_dir(path)?
             .map(|res| res.map(|e| e.path()))
             .collect::<Result<Vec<_>, io::Error>>()?;
-        let (dir_paths, file_paths): (Vec<_>, Vec<_>) = entries.into_iter().partition(|path| path.is_dir());
+        let (dir_paths, file_paths): (Vec<_>, Vec<_>) =
+            entries.into_iter().partition(|path| path.is_dir());
         let files = file_paths
             .into_iter()
             .map(|file_path| {
@@ -63,7 +73,7 @@ impl Node {
                         file_path.clone(),
                         BTreeSet::new(),
                         BTreeSet::new(),
-                        Metadata::new(),
+                        FileMetadata::new(&file_path),
                     )),
                 )
             })
@@ -72,12 +82,12 @@ impl Node {
             .into_iter()
             .map(|dir| (dir.clone(), Node::new(dir.clone()).unwrap()))
             .collect::<HashMap<PathBuf, Node>>();
-        return Ok(Node {
+        Ok(Node {
             files,
             tags: HashMap::new(),
             node_tags: BTreeSet::new(),
             directories,
-        });
+        })
     }
 
     pub fn add_node_tag(&mut self, tag: &'static Tag) -> bool {
@@ -98,21 +108,67 @@ pub struct Tag {
 }
 
 #[derive(Debug)]
-pub struct Metadata {
-    created: DateTime<Utc>,
-    modified: DateTime<Utc>,
+pub struct FileMetadata {
     size: u64,
-    extension: String,
+    readonly: bool,
+    modified: Option<DateTime<Utc>>,
+    accessed: Option<DateTime<Utc>>,
+    created: Option<DateTime<Utc>>,
+    unix: Option<UnixMetadata>,
+    windows: Option<WindowsMetadata>,
 }
 
-impl Metadata {
-    pub fn new() -> Self {
-        //[!] Mock - Metadata will be read from File System
-        Metadata {
-            created: Utc::now(),
-            modified: Utc::now(),
-            size: 0,
-            extension: String::new(),
+#[derive(Debug)]
+struct UnixMetadata {
+    permissions: u32,
+    uid: u32,
+    gid: u32,
+}
+
+#[derive(Debug)]
+struct WindowsMetadata {
+    attributes: u32,
+}
+
+impl FileMetadata {
+    pub fn new(path: &PathBuf) -> Self {
+        let meta = match fs::metadata(path) {
+            Ok(meta) => meta,
+            Err(_) => {
+                return FileMetadata {
+                    size: 0,
+                    readonly: false,
+                    modified: None,
+                    accessed: None,
+                    created: None,
+                    unix: None,
+                    windows: None,
+                }
+            }
+        };
+
+        FileMetadata {
+            size: meta.len(),
+            readonly: meta.permissions().readonly(),
+            modified: meta.modified().ok().map(DateTime::<Utc>::from),
+            accessed: meta.accessed().ok().map(DateTime::<Utc>::from),
+            created: meta.created().ok().map(DateTime::<Utc>::from),
+            
+            #[cfg(unix)]
+            unix: Some(UnixMetadata {
+                permissions: meta.mode(),
+                uid: meta.uid(),
+                gid: meta.gid(),
+            }),
+            #[cfg(windows)]
+            unix: None,
+
+            #[cfg(windows)]
+            windows: Some(WindowsMetadata {
+                attributes: meta.file_attributes(),
+            }),
+            #[cfg(unix)]
+            windows: None,
         }
     }
 }
@@ -121,7 +177,7 @@ impl Metadata {
 pub struct File {
     path: PathBuf,
     hash: u64,
-    metadata: Metadata,
+    metadata: FileMetadata,
     tags: BTreeSet<&'static Tag>,
     dir_tags: BTreeSet<&'static Tag>,
 }
@@ -145,7 +201,7 @@ impl File {
         path: PathBuf,
         tags: BTreeSet<&'static Tag>,
         dir_tags: BTreeSet<&'static Tag>,
-        metadata: Metadata,
+        metadata: FileMetadata,
     ) -> Self {
         File {
             path,
